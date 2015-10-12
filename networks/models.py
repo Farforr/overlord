20f5ca034d6ec0fdc7ecc569d5ad1af2f9deff7a
Cory Perkins <Link2cory@gmail.com>
2015-10-11 19:02:46 -0700

    added the networks app which contains all data for networks

---
+++ networks/models.py
@@ -0,0 +1,14 @@
+from core.models import DatedModel, NamedModel
+from django.core.validators import ValidationError
+
+
+class Network(DatedModel, NamedModel):
+
+    def validate_unique(self, *args, **kwargs):
+        super(Network, self).validate_unique(*args, **kwargs)
+
+        old_network = self.__class__.objects.filter(
+            name=self.name, owner=self.owner.pk)
+
+        if old_network.exists():
+            raise ValidationError("Network with that name already exists")
