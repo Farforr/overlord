b248b09cf7b1717998a5ec2cc9cd11255dfd4ec4
Cory Perkins <Link2cory@gmail.com>
2015-10-11 19:15:07 -0700

    added the devices app to deal with device logic and storage

---
+++ devices/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from core.models import DatedModel, NamedModel
+
+class Node(DatedModel, NamedModel):
+    network = models.ForeignKey(Network)
+
+
+class SensorType(DatedModel, NamedModel):
+    manufacturer = models.CharField(max_length=45)
+    units = models.CharField(max_length=10)
+
+
+class Sensor(DatedModel, NamedModel):
+    node = models.ForeignKey(Node)
+    model = models.ForeignKey(SensorType)
+
+
+class ActuatorType(DatedModel, NamedModel):
+    manufacturer = models.CharField(max_length=45)
+
+
+class Actuator(DatedModel, NamedModel):
+    node = models.ForeignKey(Node)
+    model = models.ForeignKey(ActuatorType)
+
+
+class SensorData(DatedModel):
+    value = models.IntegerField()
+    sensor = models.ForeignKey(Sensor)
+
+
+class ActuatorData(DatedModel):
+    value = models.IntegerField()
+    actuator = models.ForeignKey(Actuator)
