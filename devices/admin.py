45ce2b8914c2ef867ff4c2e92def2a7b107c8999
Cory Perkins <Link2cory@gmail.com>
2015-10-12 22:09:12 -0700

    registered devices and networks models with the admin

--- devices/admin.py
+++ devices/admin.py
@@ -1,3 +1,83 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Node, SensorType, ActuatorType, Sensor, Actuator, SensorData, ActuatorData
+
+"""Inlines"""
+
+
+class NodeInline(admin.TabularInline):
+    model = Node
+    extra = 3
+
+
+class SensorInline(admin.TabularInline):
+    model = Sensor
+    extra = 3
+
+
+class ActuatorInline(admin.TabularInline):
+    model = Actuator
+    extra = 3
+
+
+class SensorDataInline(admin.TabularInline):
+    model = SensorData
+    extra = 0
+
+
+class ActuatorDataInline(admin.TabularInline):
+    model = ActuatorData
+
+"""End Inlines"""
+
+
+class abstractAdmin(admin.ModelAdmin):
+    readonly_fields = ['created', 'last_modified']
+    fieldsets = [
+        ('Basic Information', {'fields': ['name']}),
+        ('Date Information', {
+            'fields': ['created', 'last_modified'], 'classes': ['collapse']}),
+    ]
+
+class NodeAdmin(abstractAdmin):
+    fieldsets = [
+        ('Basic Information', {'fields': ['name', 'network']}),
+        ('Date Information', {
+            'fields': ['created', 'last_modified'], 'classes': ['collapse']}),
+    ]
+    list_display = ['name', 'network', 'created', 'last_modified']
+    inlines = [SensorInline, ActuatorInline]
+    list_filter = ['network']
+    search_fields = ['name', 'network']
+
+
+class SensorAdmin(abstractAdmin):
+    fieldsets = [
+        ('Basic Information', {'fields': ['name', 'model', 'node']}),
+        ('Date Information', {
+            'fields': ['created', 'last_modified'], 'classes': ['collapse']}),
+    ]
+    list_display = ['name', 'node', 'created', 'last_modified']
+    inlines = [SensorDataInline]
+    list_filter = ['node']
+    search_fields = ['name', 'node']
+
+
+class ActuatorAdmin(abstractAdmin):
+    fieldsets = [
+        ('Basic Information', {'fields': ['name', 'model', 'node']}),
+        ('Date Information', {
+            'fields': ['created', 'last_modified'], 'classes': ['collapse']}),
+    ]
+    list_display = ['name', 'node', 'created', 'last_modified']
+    inlines = [ActuatorDataInline]
+    list_filter = ['node']
+    search_fields = ['name', 'node']
+
+admin.site.register(Node, NodeAdmin)
+admin.site.register(Sensor, SensorAdmin)
+admin.site.register(Actuator, ActuatorAdmin)
+admin.site.register(SensorType)
+admin.site.register(ActuatorType)
+admin.site.register(SensorData)
+admin.site.register(ActuatorData)
--- networks/admin.py
+++ networks/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Network
+from devices.admin import NodeInline
+
+
+class abstractAdmin(admin.ModelAdmin):
+    readonly_fields = ['created', 'last_modified']
+    fieldsets = [
+        ('Basic Information', {'fields': ['name']}),
+        ('Date Information', {
+            'fields': ['created', 'last_modified'], 'classes': ['collapse']}),
+    ]
+
+
+class NetworkAdmin(abstractAdmin):
+    inlines = [NodeInline]
+
+
+admin.site.register(Network, NetworkAdmin)
